82f7e6d5d907c746a3c9903e5525b3402f10c120
一只程序猿 <97828287+ProgramerLiang@users.noreply.github.com>
2023-09-16 14:43:32 +0800

    创建Linux端环境配置文件

---
+++ 环境配置.sh
@@ -0,0 +1,5 @@
+sudo apt install pip
+pip uninstall numpy
+pip install --index-url https://pypi.tuna.tsinghua.edu.cn/simple numpy
+pip install PyPrind-2.11.2.tar.gz
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple  opencv-python
